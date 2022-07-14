bbc0ef9488f50af6c3851937d40c6b6c315ab79e
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-14 14:57:28 +0900

    프론트 javascript 만듬

--- app/app.js
+++ app/app.js
@@ -9,5 +9,6 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // -> 미들 웨어 등록 메소드
+app.use(express.static(`${__dirname}/src/public`))
 
 module.exports = app;
