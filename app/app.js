eb48223caff800e0d27062968799c014f5d040b4
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-14 14:51:18 +0900

    setting 2

---
+++ app/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const home = require("./src/routes/home");
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // -> 미들 웨어 등록 메소드
+
+module.exports = app;
