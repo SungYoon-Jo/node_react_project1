ec492927f1b7286a38080b1b7449ed70aedbce00
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-15 12:17:14 +0900

    body-parser

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,17 +7,17 @@ const id = document.querySelector("#id"),
 
     loginbtn.addEventListener("click", login)
 
-    function login() {
-        const req = {
-            id: id.value,
-            passwd: passwd.value,
-        };
+function login() {
+    const req = {
+        id: id.value,
+        passwd: passwd.value,
+    };
 
-        fetch("/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/josn"
-            },
-            body: JSON.stringify(req),
-        });
-    }
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+}
