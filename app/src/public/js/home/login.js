fa507ec1a7faea2a36af1c65fb345b85f5e35c7b
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-15 11:44:28 +0900

    login.js 설정

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("hello");
+
+const id = document.querySelector("#id"),
+    passwd = document.querySelector("#passwd"),
+    loginbtn = document.querySelector("button");
+
+    loginbtn.addEventListener("click", login)
+
+    function login() {
+        const req = {
+            id: id.value,
+            passwd: passwd.value,
+        };
+
+        fetch();
+    }
