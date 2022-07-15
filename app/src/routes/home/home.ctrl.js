7c7900dae47a8ce7d0d7cfbe7cf2a67a2de65a1b
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-15 13:40:08 +0900

    back login

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -4,14 +4,33 @@ const output = {
     home: (req, res) => {
         res.render("home/index");
     },
-    login: (req, res)=>{
+    login: (req, res) => {
         res.render("home/login");
     },
 };
 
+const users = {
+    id: ["1", "2", "3"],
+    passwd: ["4", "5", "6"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            passwd = req.body.passwd;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.passwd[idx] === passwd) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "login no success",
+        });
     },
 };
 
