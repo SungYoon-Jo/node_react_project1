517ba12a85552757c5b4ac51120b91f1680eae93
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-18 11:24:53 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -30,6 +30,14 @@ class UserStroage {
 
         return userInfo;
     }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.passwd.push(userInfo.passwd);
+        return { success: true };
+    }
 }
 
 
