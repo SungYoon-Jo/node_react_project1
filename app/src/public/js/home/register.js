4b8e769ae824939306dd76b86cca15a1f69dd19e
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-18 11:04:36 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,23 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     passwd = document.querySelector("#passwd"),
-    loginbtn = document.querySelector("#button");
+    confirmPasswd = document.querySelector("#confirm-passwd"),
+    registerbtn = document.querySelector("#button");
 
-    loginbtn.addEventListener("click", login)
+    registerbtn.addEventListener("click", register)
 
-function login() {
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         passwd: passwd.value,
+        confirmPasswd: confirmPasswd.value,
+
     };
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -22,12 +27,12 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
         if(res.success) {
-            location.href = "/";
+            location.href = "/login";
         } else {
             alert(res.msg);
         }
     })
     .catch((err) => {
-        console.error(new Error("로그인 중 에러 발생"));
+        console.error(new Error("회원가입 중 에러 발생"));
     });
 }
