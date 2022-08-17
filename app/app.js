50c776c95675d3733cd3ca9f4b0d3341798ed63a
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-17 11:34:22 +0900

    log 생성

--- app/app.js
+++ app/app.js
@@ -3,11 +3,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require('morgan');
+
+
 
 const app = express();
+dotenv.config();
 
 const home = require("./src/routes/home");
+
+const accessLogStream = require("./src/config/log")
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
@@ -15,6 +20,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(morgan('common', { stream: accessLogStream }));
 
 app.use("/", home); // -> 미들 웨어 등록 메소드
 
